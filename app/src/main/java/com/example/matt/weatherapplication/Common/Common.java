b1b51bf1f64647642800d6379b16988f1aa6a4bc
Matt Palmer <mlpalmer99@gmail.com>
2018-07-31 22:40:32 +1000

    Initial commit

---
+++ app/src/main/java/com/example/matt/weatherapplication/Common/Common.java
@@ -0,0 +1,32 @@
+package com.example.matt.weatherapplication.Common;
+
+import android.support.annotation.NonNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Common {
+    public static String API_KEY = "48728ba4949faaaeef6d1ea5d3e675bc";
+    public static String API_LINK = "http://api.openweathermap.org/data/2.5/weather";
+
+    @NonNull
+    public static String apiRequest(String lat, String lng){
+        StringBuilder sb = new StringBuilder(API_LINK);
+        sb.append(String.format("?lat=%s&lon=%s&APPID=%s&units=metric", lat, lng, API_KEY));
+        return sb.toString();
+    }
+
+    public static String unixTimeStampToDateTime(double unixTimeStamp){
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date ();
+        date.setTime((long)unixTimeStamp*1000);
+        return dateFormat.format(date);
+    }
+
+    public static String getImage(String icon){
+        return String.format("http://openweathermap.org/img/w/%s.png", icon);
+    }
+
+
+}
